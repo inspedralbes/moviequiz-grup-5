9fc1abbdfafcc32b3d1c16d84edc75d89028615d
grefusaa <a20marsolluc@inspedralbes.cat>
2021-12-02 13:26:35 +0100

    creacio de javascript i login con simo

--- index.php
+++ index.php
@@ -26,13 +26,13 @@
             </div>
             <div class="input-field col s3 m3 ">
                 <i class="material-icons prefix">account_circle</i>
-                <input id="icon_prefix" type="text" class="validate">
-                <label for="icon_prefix">Usuario</label>
+                <input id="username" type="text" class="validate">
+                <label for="username">Usuario</label>
             </div>
             <div class="input-field col s3 m3 ">
                 <i class="material-icons prefix">https</i>
-                <input id="icon_telephone" type="tel" class="validate">
-                <label for="icon_telephone">Contraseña</label>
+                <input id="pwd" type="tel" class="validate">
+                <label for="pwd">Contraseña</label>
             </div>
             <div class="input-field col s3 m3 ">           
                 <button class="btn waves-effect waves-light" type="submit" name="action">Entrar
@@ -46,48 +46,14 @@
   </nav>
   <div class="row">
       
-      <div id="resultados" class="col s6">
+      <div id="resultados" class="col s8">
         <h1>Peliculas</h1>
       </div>
-      <div class="col s6">
+      <div class="col s4">
     </div>
       <div class="col s12">This div is 12-columns wide on all screen sizes</div>
     </div>
 
-
-
-    <script>
-        document.getElementById("busqueda").addEventListener("click",function(){
-        let busqueda=document.getElementById("busquedaPeli").value;
-            
-
-            fetch(`http://www.omdbapi.com/?s=${busqueda}&apikey=93763d43`).then(function(res){
-                return res.json();
-            }).then(function(data){
-                    console.log(data);
-                    let htmlStr="";
-                    for (let index = 0; index < 10; index++) {
-
-                        htmlStr += `<div id="contenedor" class="row">
-                                        <div class="col s6 m3">
-                                            <div id="pelicula" class="card">
-                                            <div class="card-image">
-                                            <img src="${data.Search[index].Poster}" width="50px">
-                                            <span class="card-title">${data.Search[index].Title}</span>
-                                            <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
-                                            </div>
-                                            <div class="card-content">
-                                            <p>${data.Search[index].Year}</p>
-                                            </div>
-                                            </div> 
-                                        </div>
-                                    </div>                           
-                                    `;
-             
-                    }
-                    document.getElementById("resultados").innerHTML = htmlStr;
-            });
-        });
-    </script>
+    <script type="text/javascript" src="javascript.js"></script>
 </body>
 </html>
---
+++ javascript.js
@@ -0,0 +1,60 @@
+let u = document.getElementById("username").value;
+
+let p = document.getElementById("pwd").value;
+
+
+            const datosEnvio = new FormData();
+
+            datosEnvio.append('username', u);
+
+            datosEnvio.append('pwd', p);
+
+
+fetch(`https://labs.inspedralbes.cat/~aperezh/login2.php`, {
+
+                    method: 'POST',
+
+                    body: datosEnvio
+
+                })
+
+                .then(response => response.json())
+
+                .then(data => {
+
+                    console.log(data);
+                });
+
+
+document.getElementById("busqueda").addEventListener("click",function(){
+    let busqueda=document.getElementById("busquedaPeli").value;
+        
+
+        fetch(`http://www.omdbapi.com/?s=${busqueda}&apikey=93763d43`).then(function(res){
+            return res.json();
+        }).then(function(data){
+                console.log(data);
+                let htmlStr="";
+                for (let index = 0; index < 10; index++) {
+
+                    htmlStr += `<div id="contenedor" class="row">
+                                    <div class="col s6 m3">
+                                        <div id="pelicula" class="card">
+                                        <div class="card-image">
+                                        <img src="${data.Search[index].Poster}" width="50px">
+                                        <span class="card-title">${data.Search[index].Title}</span>
+                                        <a class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+                                        </div>
+                                        <div class="card-content">
+                                        <p>${data.Search[index].Year}</p>
+                                        </div>
+                                        </div> 
+                                    </div>
+                                </div>                           
+                                `;
+         
+                }
+                document.getElementById("resultados").innerHTML = htmlStr;
+        });
+    });
+
