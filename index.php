da08d0273a2b631ed1b82e3bf407691b4656d157
grefusaa <a20marsolluc@inspedralbes.cat>
2021-12-02 10:11:34 +0100

    index y logo

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=ç, initial-scale=1.0">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+</head>
+<body>
+<div class="row">
+      <div class="col s12">
+            <img src="img/logo.png" width="50px">
+            <label for="username">Usuario</label>
+            <input type="text" id="usernsame" name="username"/>
+            <label for="username">Contraseña</label>
+            <input type="text" id="contraseña" name="contraseña"/>
+      </div>
+      <div class="col s12">12-columns </div>
+
+</div> 
+
+</body>
+</html>
