997bd96bb783871f52413927f99439a01f5f1f63
a20mohwafche <a20mohwafche@inspedralbes.cat>
2021-12-20 13:12:06 +0100

    arreglo de queryselector

--- javascript.js
+++ javascript.js
@@ -174,9 +174,9 @@ document.getElementById("busqueda").addEventListener("click",function(){
                     if (e.target.classList.contains("Guardar-valoracion")) {
                         
 
-                        let favorito = e.target.parentElement.querySelector("[name='Favorito']".checked == true) ? 1 : 0;
+                        let favorito = e.target.parentElement.querySelector("[name='Favorito'].checked" == true) ? 1 : 0;
                         let comentario = document.getElementById("comentario").value;
-                        let valoracion = e.target.parentElement.querySelector("[name='valoracion']:checked").value; 
+                        let valoracion = e.target.parentElement.querySelectorAll("[name='valoracion']:checked").value; 
                         let user = document.getElementById("username").value;
                         const numPelis = e.target.value;
                         console.log("Añado la pelicula " + numPelis);
