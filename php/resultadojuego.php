ce87d244d92f44ffea73d21ecbd22d82614f9694
grefusaa <a20marsolluc@inspedralbes.cat>
2021-12-21 17:36:38 +0100

    idk

--- php/resultadojuego.php
+++ php/resultadojuego.php
@@ -1,5 +1,21 @@
 <?php
-include ("Connect.php");
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+define('USER', 'a20marsolluc_admin');
+define('PASSWORD', 'Admin1234');
+define('HOST', 'localhost');
+define('DATABASE', 'a20marsolluc_moviequiz');
+
+try {
+    $connection = new PDO("mysql:host=".HOST.";dbname=".DATABASE, USER, PASSWORD);
+} catch (PDOException $e) {
+    exit("Error: " . $e->getMessage());
+}
+
+
+
 $data = file_get_contents("./../json/recibir_manual.json");
 $resultat = json_decode($data, true);
 
